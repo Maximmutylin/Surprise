4a0bb93cfa40fcc4c1de1c861698d1c1a805d731
Maximmutylin <87900810+Maximmutylin@users.noreply.github.com>
2022-03-18 20:51:49 +0300

    Ready project

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict'
+window.onload = function() {
+    const heart = document.querySelector('.heart');
+    const container = document.querySelector('.first-screen');
+  
+    function animationHeart() {
+        const countdownContainer = document.createElement('div');
+        container.append(countdownContainer);
+        countdownContainer.classList.add("first-title");
+
+        let counter = 3;
+        let timer = setInterval(() => {
+            countdownContainer.innerHTML = `Магия начнется через
+            ${counter--}`;
+            }, 1000);
+        setTimeout(() => { clearInterval(timer);  }, 5000);
+        
+        heart.classList.add('heart__active');
+    }
+    heart.addEventListener('click', animationHeart);
+
+    const photos = document.querySelectorAll('.none');
+
+    function animationFirstPhoto(){
+        container.style = "display:none";
+        photos[0].classList.add('first-img');
+        photos[0].addEventListener('animationend', function() {
+            photos[0].classList.add('visible-first-img');
+            setTimeout(() => {
+                photos[0].style = 'display: none;';
+            }, 5000);
+            photos[0].addEventListener('animationend', function() {
+                photos[1].classList.add('second-img');
+                photos[1].addEventListener('animationend', function(){
+                    setTimeout(() => {
+                        photos[1].classList.add('visible-second-img')
+                        setTimeout(() => {
+                            photos[1].style = 'display: none;';
+                        }, 3000);
+                    }, 2000);  
+                })
+                photos[1].addEventListener('animationend', function(){
+                    photos[2].classList.add('third-img');
+                    photos[2].addEventListener('animationend', function() {
+                        photos[2].style = 'display: none;';
+                    })
+                    photos[2].addEventListener('animationend', function() {
+                        const finalText = document.querySelector('.text');
+                        finalText.classList.add('text__animation');
+                    })
+                })
+            })
+        })
+    }
+    heart.addEventListener('animationend', animationFirstPhoto);
+    
+}
